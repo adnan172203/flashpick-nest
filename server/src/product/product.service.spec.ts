1b3e9ce3cbbe11c7ad0b020efc6a2a770487fa0e
adnan <adnan172203@gmail.com>
2024-06-06 18:17:20 +0600

    added new test for product creation and passed

--- server/src/product/product.service.spec.ts
+++ server/src/product/product.service.spec.ts
@@ -1,18 +1,111 @@
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { ProductService } from './product.service';
+
+// describe('ProductService', () => {
+//   let service: ProductService;
+
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       providers: [ProductService],
+//     }).compile();
+
+//     service = module.get<ProductService>(ProductService);
+//   });
+
+//   it('should be defined', () => {
+//     expect(service).toBeDefined();
+//   });
+// });
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductService } from './product.service';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { NotFoundException } from '@nestjs/common';
+import { Product } from './entities/product.entity';
 
-describe('ProductService', () => {
+// type MockRepositoryType<T = any> = Partial<
+//   Record<keyof Repository<T>, jest.Mock>
+// >;
+
+const mockRepository = {
+  create: jest.fn(),
+  save: jest.fn(),
+  findOneBy: jest.fn(),
+  find: jest.fn(),
+  remove: jest.fn(),
+  findOne: jest.fn(),
+};
+
+// const mockProduct = {
+//   id: '1',
+//   name: 'product one',
+//   description: 'product one description',
+//   price: 2,
+//   quantity: 2,
+//   sku: '12',
+//   color: 'red',
+//   size: 'sdf',
+//   stock: 12,
+//   status: true,
+//   fullDescription: 'sdf',
+//   additionalText: 'sdf',
+//   images: [{ imageUrl: 'three.jpg' }, { imageUrl: 'three-another.jpg' }],
+//   productId: "123"
+// };
+
+const mockProduct = {
+  id: '1',
+  name: 'product one',
+  description: 'product one description',
+  price: 2,
+  quantity: 2,
+  sku: '12',
+  color: 'red',
+  size: 'sdf',
+  stock: '12',
+  status: true,
+  fullDescription: 'sdf',
+  additionalText: 'sdf',
+};
+
+describe('Product Service', () => {
+  // const mockRepository = {
+  //   create: jest.fn().mockReturnValue({ ...productParams }),
+  //   save: jest.fn().mockReturnValue({ ...productParams }),
+  // };
+
   let service: ProductService;
+  // let productRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductService],
+      providers: [
+        ProductService,
+        {
+          provide: getRepositoryToken(Product),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ProductService>(ProductService);
+    // productRepository = module.get(getRepositoryToken(Product));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create Product', () => {
+    describe('when pass product params', () => {
+      it('should create a new product', async () => {
+        mockRepository.save.mockReturnValue(mockProduct);
+
+        const savedProduct = await service.createProduct(mockProduct);
+
+        expect(savedProduct).toBe(mockProduct);
+      });
+    });
   });
 });
--- server/src/product/product.service.ts
+++ server/src/product/product.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+
+interface ProductParams {
+  name: string;
+  description: string;
+  price: number;
+  quantity: number;
+  sku: string;
+  color?: string;
+  size?: string;
+  stock: string;
+  status: boolean;
+  fullDescription: string;
+  additionalText: string;
+}
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ) {}
+
+  async createProduct({ ...product }: ProductParams) {
+    const newProduct = this.productRepository.create(product);
+
+    const savedProduct = await this.productRepository.save(newProduct);
+
+    return savedProduct;
+  }
+}
