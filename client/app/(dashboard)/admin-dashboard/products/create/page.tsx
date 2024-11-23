9c8663c9162c5b0b4e0f571604783277ff605d7e
adnan <adnan172203@gmail.com>
2024-11-23 19:50:08 +0600

    feat: Add product creation page and styling
    Implement a new page for creating products.
    Include a rich text editor for detailed descriptions.
    Add custom styling for improved user experience.

--- client/app/(dashboard)/admin-dashboard/products/create/page.tsx
+++ client/app/(dashboard)/admin-dashboard/products/create/page.tsx
@@ -1,7 +1,12 @@
+import AddProduct from '@/components/features/Dashboard/Add-Product/Add-Product';
 import React from 'react';
 
 const CreateProducts = () => {
-  return <div>CreateProducts</div>;
+  return (
+    <div>
+      <AddProduct />
+    </div>
+  );
 };
 
 export default CreateProducts;
